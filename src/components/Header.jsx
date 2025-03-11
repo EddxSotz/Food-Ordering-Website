3ec9573f1e8fa67390a0fb77be097925b84a657e
Edy S. <edysoto56@gmail.com>
2025-03-10 22:58:13 -0600

    create Header file component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import logo from '../logo.jpg';
+
+function Header() {
+  return (
+    <header id="main-header">
+      <h1 id="title">My Blog</h1>
+      <img src={logo} alt="Logo" /> 
+        <nav>           
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+       
+    </header>
+  );
+}
+export default Header;
