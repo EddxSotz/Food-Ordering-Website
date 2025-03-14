7f48bf7904514bc30570def5fc67e4a975f0ed72
Edy S. <edysoto56@gmail.com>
2025-03-13 23:13:14 -0600

    Implement modal for cart display and add item removal functionality

--- src/components/CartContents.jsx
+++ src/components/CartContents.jsx
@@ -3,13 +3,16 @@ import {useContext} from 'react';
 import CartContext from '../store/CartContext.jsx';
 import {currencyFormatting} from '../utils/currency-formatting.js';
 
-export default function CartContents({onClose}) {
+export default function CartContents() {
     const cartCtx = useContext(CartContext);
     const cartTotal = cartCtx.cartItems.reduce((total, item) => {
         return total + item.price * item.quantity;
     }
     , 0);
 
+    const handleRemoveItem = (id) => {
+        cartCtx.removeItem(id);
+    }
     return (
         <Modal>
             <h2>Cart Contents</h2>
@@ -19,6 +22,7 @@ export default function CartContents({onClose}) {
                         <span>{item.name}</span>
                         <span>{item.quantity}</span>
                         <span>{item.price}</span>
+                        <button onClick={()=> handleRemoveItem(item.id)}>Remove Item</button>
                     </li>                    
                 ))}
                 <p>Total: {currencyFormatting(cartTotal)}</p>
