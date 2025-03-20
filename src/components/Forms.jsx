2c7460f21b3c854e783b01cba89a3cb7b3a47f20
Edy S. <edysoto56@gmail.com>
2025-03-19 22:18:20 -0600

    Refactor shipping form to use FormData and enhance styling

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,48 +1,35 @@
-import {useContext, useState} from 'react';
+import {} from 'react';
 
-export default function Forms({ onSubmit }) {
-    const [formData, setFormData] = useState({
-        name: '',
-        address: '',
-        city: '',
-        postalCode: '',
-        country: ''
-    });
+export default function Forms({ onSubmit }) {    
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(formData);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const inputData = Object.fromEntries(formData.entries());
+        onSubmit(inputData);        
     };
 
     return (
         <form onSubmit={handleSubmit} id='shipping-form'>
             <div className='input-field'>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+                <input type="text" id="name" name="name" required />
             </div>
             <div className='input-field'>
                 <label htmlFor="address">Address:</label>
-                <input type="text" id="address" name="address" value={formData.address} onChange={handleChange} required />
+                <input type="text" id="address" name="address" required />
             </div>
             <div className='input-field'>
                 <label htmlFor="city">City:</label>
-                <input type="text" id="city" name="city" value={formData.city} onChange={handleChange} required />
+                <input type="text" id="city" name="city" required />
             </div>
             <div className='input-field'>
                 <label htmlFor="postalCode">Postal Code:</label>
-                <input type="text" id="postalCode" name="postalCode" value={formData.postalCode} onChange={handleChange} required />
+                <input type="text" id="postalCode" name="postalCode" required />
             </div>
             <div className='input-field'>
                 <label htmlFor="country">Country:</label>
-                <input type="text" id="country" name="country" value={formData.country} onChange={handleChange} required />
+                <input type="text" id="country" name="country" required />
             </div>
             <button type="submit" className='cart-button'>Submit</button>
         </form>
