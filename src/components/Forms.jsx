066b109757911eec347ea3a4b70d1f46cf72e5f1
Edy S. <edysoto56@gmail.com>
2025-03-20 22:51:40 -0600

    Fix error on form submit on backend

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -36,6 +36,10 @@ export default function Forms({ onSubmit }) {
                 <input type="text" id="name" name="name" required />
             </div>
             <div className='input-field'>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+            </div>
+            <div className='input-field'>
                 <label htmlFor="address">Address:</label>
                 <input type="text" id="address" name="address" required />
             </div>
