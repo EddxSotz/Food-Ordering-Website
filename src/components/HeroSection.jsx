8527dcbc0be8dff6ec74d1165bf54de0b2f783d4
Edy S. <edysoto56@gmail.com>
2025-04-06 23:57:45 -0600

    Style changes on Navbar and Hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,6 @@
-
 function HeroSection() {
   return (
-    <div className="hero-section">
+    <div className={`bg-[url("/src/assets/background.jpg")] bg-cover h-screen bg-center flex flex-col justify-center items-center text-white`}>
       <h1>Fresh & Tasty Meals</h1>
       <p>Your all-in-one place for your food service needs</p>
       <button className="cta-button">Shop Menu</button>
