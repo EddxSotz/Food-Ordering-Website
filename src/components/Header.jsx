8fd623f5cceb610bb0089ac3ef43b656c29fc3dc
Edy S. <edysoto56@gmail.com>
2025-04-08 00:00:00 -0600

    Enhance Header and HeroSection styles for improved UI and accessibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,19 +17,19 @@ function Header() {
 
 
   return (
-    <section className='fixed top-0 flex flex-col items-center text-stone-50 w-full py-2 bg-gray-600 bg-opacity-70 backdrop-blur-sm z-10'>
+    <section className='fixed top-0 flex flex-col items-center text-stone-50 w-full py-2 bg-linear-to-b from-emerald-950/85 to-emerald-950/45 z-10'>
     <header className='w-4/5 mx-auto flex flex-row justify-between items-center'>
       <div>
         <img src={logo} alt="Logo" className='w-12 inline mr-2'/>
         <h1 className='inline text-lg font-semibold'>Le Fancy Restaurant</h1>        
       </div>       
       <nav id="navbar">           
-          <ul className='flex flex-col md:flex-row gap-4 font-semibold'>
-            <li><NavLink to="/" className={({ isActive }) => isActive ? "nav-item-active" : "nav-item"}>Home</NavLink></li>
-            <li><NavLink to="/shop" className={({ isActive }) => isActive ? "nav-item-active" : "nav-item"}>Shop</NavLink></li>
-            <li><NavLink to="/about" className={({ isActive }) => isActive ? "nav-item-active" : "nav-item"}>About</NavLink></li>
-            <li><NavLink to="/contact" className={({ isActive }) => isActive ? "nav-item-active" : "nav-item"}>Contact</NavLink></li>
-            <button onClick={() => setIsModalOpen(true)} className='cart-button'>Cart <span>{totalCartItems}</span></button>
+          <ul className='flex flex-col md:flex-row gap-4 font-semibold items-center'>
+            <li><NavLink to="/" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:bg-lime-800"}>Home</NavLink></li>
+            <li><NavLink to="/shop" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-800"}>Shop</NavLink></li>
+            <li><NavLink to="/about" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-800"}>About</NavLink></li>
+            <li><NavLink to="/contact" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-800"}>Contact</NavLink></li>
+            <button onClick={() => setIsModalOpen(true)} className='bg-stone-50 text-gray-700 hover:bg-lime-800 hover:text-stone-50 hover:cursor-pointer rounded-md p-4'>Cart <span>{totalCartItems}</span></button>
           </ul>          
       </nav>      
     </header>
