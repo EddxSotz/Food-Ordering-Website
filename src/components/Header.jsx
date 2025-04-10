df3aea3923634fc4230099643cff95c934110614
Edy S. <edysoto56@gmail.com>
2025-04-10 03:01:52 -0600

    Update modal  & Header behavior on Mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,11 @@ function Header() {
   }, 0);
 
 
+  const handleOpenCart = () => {
+    setIsModalOpen(true);
+    setNavToggle(false)
+  }
+
   return (
     <section className='fixed top-0 flex flex-col items-center text-stone-50 w-full py-2 bg-emerald-950/85 z-10'>
       <header className='w-4/5 mx-auto flex flex-row justify-between items-center'>
@@ -33,12 +38,12 @@ function Header() {
           {navToggle && (
               <nav className='md:hidden absolute top-16 right-0 bg-emerald-950/85 w-1/2 h-screen shadow-md'>           
               <ul className='flex flex-col md:flex-row gap-4 font-semibold items-center'>
-                <li><NavLink to="/" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-800"}>Home</NavLink></li>
-                <li><NavLink to="/shop" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-600"}>Shop</NavLink></li>
-                <li><NavLink to="/about" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-600"}>About</NavLink></li>
-                <li><NavLink to="/contact" className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-600"}>Contact</NavLink></li>
-                <li><NavLink to="/login" className={({ isActive }) => isActive ? "bg-lime-800 text-stone-50 rounded-md py-1 px-2" : "bg-stone-50 text-gray-700 hover:bg-lime-800 hover:text-stone-50 hover:cursor-pointer rounded-md py-1 px-2"}>Login</NavLink></li>
-                <button onClick={() => setIsModalOpen(true)} className='bg-lime-800 hover:bg-stone-50 hover:text-lime-800 hover:cursor-pointer rounded-md py-2 px-4 ml-4'>Cart <span>{totalCartItems}</span></button>
+                <li><NavLink to="/" onClick={() => setNavToggle(false)} className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-800"}>Home</NavLink></li>
+                <li><NavLink to="/shop" onClick={() => setNavToggle(false)} className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-600"}>Shop</NavLink></li>
+                <li><NavLink to="/about" onClick={() => setNavToggle(false)} className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-600"}>About</NavLink></li>
+                <li><NavLink to="/contact" onClick={() => setNavToggle(false)} className={({ isActive }) => isActive ? "underline" : "hover:cursor-pointer hover:text-lime-600"}>Contact</NavLink></li>
+                <li><NavLink to="/login" onClick={() => setNavToggle(false)} className={({ isActive }) => isActive ? "bg-lime-800 text-stone-50 rounded-md py-1 px-2" : "bg-stone-50 text-gray-700 hover:bg-lime-800 hover:text-stone-50 hover:cursor-pointer rounded-md py-1 px-2"}>Login</NavLink></li>
+                <button onClick={handleOpenCart} className='bg-lime-800 hover:bg-stone-50 hover:text-lime-800 hover:cursor-pointer rounded-md py-2 px-4 ml-4'>Cart <span>{totalCartItems}</span></button>
               </ul>          
           </nav>
           )}                
