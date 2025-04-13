9efab402e19b35836509c69ac788227ffee730a0
Edy S. <edysoto56@gmail.com>
2025-04-13 00:30:14 -0600

    improve cart modal responsive width

--- src/UI/Modal.jsx
+++ src/UI/Modal.jsx
@@ -29,7 +29,7 @@ export default function Modal({children, openStatus, onClose}) {
 
     return createPortal(
         <div className="fixed inset-0 w-screen h-screen bg-gray-900/50 z-20">
-            <dialog ref={modalRef} className="w-3/4 sm:w-1/2 md:w-1/3 h-screen mr-0 ml-auto mb-0 bg-stone-300/95 text-gray-700 flex flex-col" onKeyDown={handleEscapeKey} onClick={handleClickOutside}>         
+            <dialog ref={modalRef} className="w-screen sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 h-screen mr-0 ml-auto mb-0 bg-stone-300/95 text-gray-700 flex flex-col" onKeyDown={handleEscapeKey} onClick={handleClickOutside}>         
             <button onClick={onClose} className="absolute top-0 right-0 text-xl font-bold hover:text-stone-50 hover:bg-gray-600 hover:cursor-pointer active:bg-gray-800 active:text-stone-50 p-4">X</button>
             {children}                  
             </dialog>,
