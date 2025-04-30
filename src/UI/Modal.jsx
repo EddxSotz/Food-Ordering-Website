17eed0d77cceb3b29844aefd0755af1e068d4965
Edy S. <edysoto56@gmail.com>
2025-04-29 23:18:06 -0600

    small fix on  bg for modal

--- src/UI/Modal.jsx
+++ src/UI/Modal.jsx
@@ -29,7 +29,7 @@ export default function Modal({children, openStatus, onClose}) {
     }
 
     return createPortal(
-        <div className="fixed inset-0 w-screen h-screen bg-gray-900/50 z-50">
+        <div className="fixed inset-0 w-screen h-screen z-50">
             <dialog ref={modalRef} className="h-screen w-auto sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 mr-0 ml-auto bg-stone-300/95" onKeyDown={handleEscapeKey} onClick={handleClickOutside}>         
             <div className="relative flex flex-col items-center justify-start w-full h-full text-gray-700">
                 <div className="w-full h-auto text-end bg-emerald-950/85">
