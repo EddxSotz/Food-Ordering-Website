482b86fb427b7d6b24a776d359da7180d204c1e1
Edy S. <edysoto56@gmail.com>
2025-07-26 22:53:02 -0600

    Add contact section with icons and message form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaPhone, FaLocationDot, FaEnvelope, FaSquareInstagram } from "react-icons/fa6";
+import ContactImage from '../assets/contact-us.webp';
 
 const Contact = () => {
     
@@ -15,7 +17,7 @@ const Contact = () => {
                 <div className='container mx-auto flex flex-row justify-between items-center'>
                     <div>
                         <p className='text-lime-600 font-semibold mb-4'>// Get in Touch</p>
-                        <h1 className="text-6xl font-bold text-white font-Zain">Shop</h1>
+                        <h1 className="text-6xl font-bold text-white font-Zain">Contact</h1>
                     </div>
                     <div>
                         <NavLink to="/" className="text-lime-600 font-semibold text-lg hover:underline">Home</NavLink>
@@ -24,39 +26,50 @@ const Contact = () => {
                     </div> 
                 </div>                               
         </div>
-        <div className="container h-auto md:h-full mx-auto p-4 pt-18 text-gray-800 text-center">
-            <h1 className="text-6xl mb-6 mt-12 font-Charm">Contact Us</h1>
-            <p className="text-xl mb-6">We'd love to hear from you! Reach out to us using the information below:</p>
-            
-            <div className="bg-gray-200 p-4 rounded-md shadow-md mb-6">
-                <h3 className="text-lg font-semibold">Address</h3>
-                <p>123 Foodie Lane, Flavor Town, FT 56789</p>
+        <div className="container h-auto md:h-full mx-auto p-4 pt-18 text-gray-800 text-center">   
+            <div className='max-h-screen grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-6 '>            
+                <div className="bg-gray-200 p-4 rounded-md shadow-md mb-6 flex flex-col items-center justify-center">
+                    <FaLocationDot className='text-4xl mb-4'/>
+                    <h2 className="text-lg md:text-xl font-semibold">Address</h2>
+                    <p>123 Foodie Lane, Flavor Town, FT 56789</p>
+                    <h3 className="text-lg font-semibold mt-6">Business Hours</h3>
+                    <p>Monday - Friday: 9:00 AM - 9:00 PM</p>
+                    <p>Saturday - Sunday: 10:00 AM - 10:00 PM</p>
+                </div>            
+                <div  className="bg-gray-200 p-4 rounded-md shadow-md mb-6 flex flex-col items-center justify-center">
+                    <FaPhone className='text-4xl mb-4'/>
+                    <h2 className="text-lg md:text-xl font-semibold">Phone</h2>                
+                    <a href="tel:+15551234567">+1 (555) 123-4567</a>
+                </div>            
+                <div  className="bg-gray-200 p-4 rounded-md shadow-md mb-6 flex flex-col items-center justify-center">
+                    <FaEnvelope className='text-4xl mb-4'/>
+                    <h2 className="text-lg md:text-xl font-semibold">Email</h2>                
+                    <a href="mailto:contact@foodorder.com">contact@foodorder.com</a>                
+                </div>                                    
+                <div  className="bg-gray-200 p-4 rounded-md shadow-md mb-6 flex flex-col items-center justify-center">
+                    <FaSquareInstagram className='text-4xl mb-4'/>
+                    <h2 className="text-lg md:text-xl font-semibold">Follow Us</h2>
+                    <p>
+                        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a> | 
+                        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"> Instagram</a> | 
+                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"> Twitter</a>
+                    </p>
+                </div>
             </div>
-            
-            <div  className="bg-gray-200 p-4 rounded-md shadow-md mb-6">
-                <h3 className="text-lg font-semibold">Phone</h3>                
-                <a href="tel:+15551234567">+1 (555) 123-4567</a>
-            </div>
-            
-            <div  className="bg-gray-200 p-4 rounded-md shadow-md mb-6">
-                <h3 className="text-lg font-semibold">Email</h3>                
-                <a href="mailto:contact@foodorder.com">contact@foodorder.com</a>                
-            </div>
-            
-            <div  className="bg-gray-200 p-4 rounded-md shadow-md mb-6">
-                <h3 className="text-lg font-semibold">Business Hours</h3>
-                <p>Monday - Friday: 9:00 AM - 9:00 PM</p>
-                <p>Saturday - Sunday: 10:00 AM - 10:00 PM</p>
-            </div>
-            
-            <div  className="bg-gray-200 p-4 rounded-md shadow-md mb-6">
-                <h3 className="text-lg font-semibold">Follow Us</h3>
-                <p>
-                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a> | 
-                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"> Instagram</a> | 
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"> Twitter</a>
-                </p>
-            </div>
+            <div className="p-6 mt-6">
+                <h1 className="text-4xl font-semibold mb-8 text-left">Send Us a Message</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>                                     
+                    <img src={ContactImage} alt="Contact Us" className="w-full h-auto rounded-md" />                  
+                <form className="flex flex-col gap-4">
+                    <h3 className="text-lg md:text-xl font-semibold text-left">Fill up the form</h3>
+                    <input type="text" placeholder="Your Name" className="p-2 border border-gray-300 rounded-md" required />
+                    <input type="email" placeholder="Your Email" className="p-2 border border-gray-300 rounded-md" required />
+                    <textarea placeholder="Your Message" className="p-2 border border-gray-300 rounded-md h-32" required></textarea>
+                    <button type="submit" className="bg-lime-700 text-white py-2 px-4 rounded-md hover:bg-lime-800">Send Message</button>
+                </form>
+                </div>
+                
+            </div>    
         </div>
         </>
     );
